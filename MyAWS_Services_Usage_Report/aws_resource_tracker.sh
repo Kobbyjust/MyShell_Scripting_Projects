b1a72341ea71a0ea516025b930ce2327cc72a3ac
Kobbyjust <sagarinokoe1@gmail.com>
2023-05-09 12:40:59 +0000

    Initial

---
+++ MyAWS_Services_Usage_Report/aws_resource_tracker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+####################
+# Author: Kobina
+# Date: 9th-May
+#
+# Version: v1
+#
+# This Script will Report the AWS Resource usage in the AWS Account
+####################
+set +x
+
+# AWS s3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+# 
+
+# List Available s3 Bucket
+echo "Print list of S3 Bucket"
+aws s3 ls > s3resourceTracker
+
+# List Available EC2
+echo "Print list of E2"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' > resourceTracker
+
+# List Lambda Functions
+echo "Print list of Lambda Functions"
+aws lambda list-functions > lambdaresourceTracker
+
+# List IAM User
+echo "Print list of IAM Users"
+aws iam list-users > iamUserresourceTracker
